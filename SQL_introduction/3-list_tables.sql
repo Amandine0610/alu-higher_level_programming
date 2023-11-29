e62405915b1ecaa2348f8fd87ed634ba57be7f49
Amandine0610 <a.irakoze@alustudent.com>
2023-11-29 02:58:20 -0800

    3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,17 +1,3 @@
 -- a script that lists all the tables of a database in my MySQL
 USE `mysql`;
-
-CREATE TABLE table_1 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-);
-
-CREATE TABLE table_2 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data TEXT
-);
-
-CREATE TABLE table_3 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    value DECIMAL(10, 2)
-);
+SHOW TABLES;
