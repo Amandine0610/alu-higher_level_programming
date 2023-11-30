4825cc8e53c2b8266927a6e3f90b1c0a7ebcecd9
Amandine0610 <a.irakoze@alustudent.com>
2023-11-30 14:20:51 -0800

    Create

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
