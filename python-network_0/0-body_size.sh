19da202d2a0ed474131eda68ed6bd1daa6b1a49c
Amandine0610 <a.irakoze@alustudent.com>
2023-11-30 15:26:00 -0800

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,2 +1,4 @@
-2
-2
+#!/bin/bash
+# This script takes a URL as an argument, sends a silent GET request using curl, and displays the size of the response body in bytes.
+
+curl -sI "$1" | grep -i 'content-length' | awk '{print $2}'
