f88b5bc0066f908fe2e1878d0ebffe99241615e3
irakozea <irakozea321@gmail.com>
2024-02-01 12:19:58 +0200

    Javaaa

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+if (ProcessingInstruction.argv[2] === undefined && Process.argv[3] === undefined) {
+ console.log("undefined is undefined");   
+} else if (Processi.argv[3] === undefined) {
+    console.log(process.argv[2] + " is undefined");
+} else {
+    console.log(process.argv[2] + " is " + process.argv[3]);
+}
