fef337e6d13640169edfc223ed23b76dabda06ae
Amandine0610 <a.irakoze@alustudent.com>
2024-02-03 01:32:52 -0800

    DOO

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
-const converter = require('./10-converter').converter;
-
-let myconv = converter(10);
-
-console.log(myconv(2));
-console.log(myconv(12));
-console.log(myconv(89));
-
-myconv = converter(16);
-
-console.log(myconv(2));
-console.log(myconv(12));
-console.log(myconv(89));
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
