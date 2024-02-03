429ddebe94b794f523a3297695d2df8f7b330314
Amandine0610 <a.irakoze@alustudent.com>
2024-02-03 00:28:46 -0800

    done

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-if (ProcessingInstruction.argv[2] === undefined && Process.argv[3] === undefined) {
- console.log("undefined is undefined");   
-} else if (Processi.argv[3] === undefined) {
-    console.log(process.argv[2] + " is undefined");
-} else {
-    console.log(process.argv[2] + " is " + process.argv[3]);
-}
+
+const Args = process.argv.slice(2);
+
+console.log(`${Args[0]} is ${Args[1]}`);
