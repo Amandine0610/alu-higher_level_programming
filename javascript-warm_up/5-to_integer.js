5c883ad156d2491da089d80f947a6e8dadb31589
Amandine0610 <a.irakoze@alustudent.com>
2024-02-03 00:31:03 -0800

    done

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (Number.isInteger(parseInt(arg))) {
-    console.log(' My number: ${parseInt(arg)}');
+
+const Args = process.argv.slice(2);
+
+if (parseInt(Args[0])) {
+  console.log(`My number: ${parseInt(Args[0])}`);
 } else {
-    console.log("Not a number");
+  console.log('Not a number');
 }
