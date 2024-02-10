b8fde580984d2c2b5ce3a5aacf7b8e7f6a372272
irakozea <irakozea321@gmail.com>
2024-02-10 21:14:40 +0200

    Done

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const args = process.argv.slice(2);
+const fs = require('fs')
+const args = process.argv.slice(2)
 
-fs.writeFile(args[0], args[1], 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
+fs.writeFile(args[0], args[1], 'utf-8', (error) => {
+  if (error) {
+    console.error(error)
   }
-});
+})
