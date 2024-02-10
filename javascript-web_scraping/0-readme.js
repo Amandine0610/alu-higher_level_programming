e1f661d09c0ce46c9dc879f17c4c55dcb5770f27
irakozea <irakozea321@gmail.com>
2024-02-11 00:33:44 +0200

    Done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2)
-const fs = require('fs')
+const args = process.argv.slice(2);
+const fs = require('fs');
 
 fs.readFile(args[0], 'utf-8', (error, data) => {
   if (error) {
-    console.error(error)
-    console.error(error)
+    console.error(error);
+    console.error(error);
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-const args = process.argv.slice(2)
+const fs = require('fs');
+const args = process.argv.slice(2);
 
 fs.writeFile(args[0], args[1], 'utf-8', (error) => {
   if (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
